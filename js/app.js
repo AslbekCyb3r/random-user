a9d6273d33c327b823a13d78bea3128853d8e05b
aslbek <rizoxonrizoxon903@gmail.com>
2026-02-26 16:53:46 +0500

    chala

--- js/app.js
+++ js/app.js
@@ -3,3 +3,100 @@ const formButton = document.getElementById('form__button')
 const user = document.getElementById('user')
 const deleteBtn = document.getElementById('delete__btn')
 const clearBtn = document.getElementById('clear__button')
+
+// refresh
+
+formButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    reload()
+    clearBtn.classList.remove('hidden')
+
+})
+
+// clear
+
+clearBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    user.innerHTML = ''
+    clearBtn.classList.add('hidden')
+})
+
+
+// form
+
+form['form__input'].addEventListener('input', () => {
+    const inputvalue = form['form__input'].value.toLocaleLowerCase()
+    const name = document.querySelectorAll('.user__name')
+    console.log(name);
+
+
+    name.forEach((item) => {
+        if (item.lastElementChild.textContent.toLocaleLowerCase().includes(inputvalue)) {
+            item.parentElement.classList.remove('hidden')
+        } else {
+            item.parentElement.classList.add('hidden')
+
+        }
+    })
+
+})
+
+const updateUi = (data) => {
+    console.log('updateui', data);
+    user.innerHTML = ''
+    data.forEach(item => {
+        const { gender, name, picture, location, dob } = item
+
+        user.innerHTML += `
+        
+        <li class="user__item">
+            <button id="delete__btn" class="user__delete--btn">
+              <i class="fas fa-trash"></i>
+            </button>
+            <img class="user__img" alt="User photo" src=${picture.large} width="100"
+              height="100" />
+            <div class="user__name">
+              <span class="material-symbols-outlined">badge</span>
+              <span>- ${name.title} ${name.first} ${name.last}</span>
+            </div>
+            <div class="user__year">
+              <span class="material-symbols-outlined">cake</span>
+              <span>- ${dob.age} years old.</span>
+            </div>
+            <div class="user__location">
+              <span class="material-symbols-outlined">person_pin_circle</span>
+              <span>-${location.city},${location.country}</span>
+            </div>
+            <div class="user__gender">
+              <span class="material-symbols-outlined">man</span>
+              <span>- ${gender}</span>
+            </div>
+          </li>
+          
+        
+        
+        `
+
+    });
+}
+
+
+
+
+document.addEventListener('click',(e)=>{
+    if(e.target.classList[0]==='.user__delete--btn'){
+        e.target.parentElement.remove()
+    }
+
+    if(!user.children.length){
+        clearBtn.classList.add('hidden')
+    }
+})
+
+
+
+
+
+
+
+
--- js/request.js
+++ js/request.js
@@ -1,19 +1,41 @@
 
 // api
 
+
 // for leader
 const overlay = document.getElementById('overlay')
 const API = 'https://randomuser.me/api/?results=9'
+
+
+
+const loaderoverfly = (toggle) => {
+    if (toggle) {
+        overlay.classList.remove('hidden')
+    } else {
+        overlay.classList.add('hidden')
+
+    }
+}
+
 
 const getdata = (resource) => {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest()
 
         request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                resolve(request.responseText)
-            } else if (request.readyState === 4) {
-                reject('error')
+            if (request.readyState < 4) {
+                // console.log('loading...');
+                loaderoverfly(true)
+
+            } else if (request.readyState === 4 && request.status == 200) {
+                const data = JSON.parse(request.responseText)
+                resolve(data.results);
+
+                loaderoverfly(false)
+            } else if (request.request == 4) {
+                reject('Eror');
+                loaderoverfly(false)
+
             }
         })
 
@@ -22,10 +44,17 @@ const getdata = (resource) => {
     })
 }
 
-getdata(API)
-    .then(data => {
-        console.log(JSON.parse(data))
-    })
-    .catch(err => {
-        console.log(err)
-    })
+
+const reload = () => {
+    getdata(API)
+        .then((data) => {
+           updateUi(data);
+
+        }).catch((err) => {
+            console.log(err);
+
+        })
+
+}
+
+document.addEventListener('DOMContentLoaded', reload)
